394f0c01c8c49d0cd67fafdef10e0114fd33132e
undernones <sjward@gmail.com>
2013-01-07 22:07:42 -0800

    Stuff

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Options.h"
 #include "Utils.h"
 #include "World.h"
+#include <geom/Mesh.h>
 
 namespace
 {
@@ -15,6 +16,8 @@ int
 main(int argc, char* argv[])
 {
     try {
+        Mesh m = Mesh::loadObj("meshes/tub.obj");
+
         Options::init(argc, argv);
 
         Material material = {
