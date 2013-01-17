f86f90261dd18c36d82a31a9ba4a42c636ff1485
undernones <sjward@gmail.com>
2013-01-17 07:19:19 -0800

    Better bunny parameters

--- examples/bunny.sh
+++ examples/bunny.sh
@@ -1,1 +1,1 @@
-gui/gui --particle_file samples/bunny40.40.40.txt --dt 5e-5 --duration 6 --mu 2500 --lambda 100 --density 10
+gui/gui --particle_file samples/bunny40.40.40.txt --dt 1e-4 --duration 6 --mu 8000 --lambda 1000 --density 10 --mesh_file meshes/bunnySimple.obj
