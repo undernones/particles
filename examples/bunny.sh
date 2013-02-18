472896f2a07d90d5f5ccc1151c1eef036059dd51
undernones <sjward@gmail.com>
2013-02-18 11:48:06 -0800

    Updated bunny example

--- examples/bunny.sh
+++ examples/bunny.sh
@@ -1,1 +1,3 @@
-gui/gui --particle_file samples/bunny40.40.40.txt --dt 5e-4 --duration 6 --mu 8000 --lambda 1000 --density 10 --mesh_file meshes/bunnySimple.obj
+#gui/gui --particle_file samples/bunny40.40.40.txt --dt 5e-4 --duration 6 --mu 8000 --lambda 1000 --density 10 --mesh_file meshes/bunnySimple.obj
+
+./headless --particle_file samples/bunny40.40.40.txt --dt 5e-4 --duration 2 --mu 8000 --lambda 1000 --density 10 --friction 0.5 --mesh_file meshes/bunnySimple.obj
